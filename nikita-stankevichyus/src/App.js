45f7c8adaa7795fd16f2f9b6fda97688634f08df
Stanoque <stanoquedzhumpsh@gmail.com>
2020-05-14 17:30:45 +0300

    Home page markup is complete.

--- nikita-stankevichyus/src/App.js
+++ nikita-stankevichyus/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import logo from './logo.png';
+import work1 from './work-1.jpg';
+import work2 from './work-2.jpg';
+import work3 from './work-3.jpg';
 import './App.css';
 import './index.scss';
 
@@ -16,6 +19,8 @@ function App() {
         <nav className='nav--horizontal container--flex justify-content--center mg-t-15'>
           <ul className = 'container--flex'>
             <li className="nav_link"><a><Icon icon='fas fa-home mg-l--15'/><span className='mg-l-5'>Home</span></a></li>
+            <li className="nav_link"><a href='#contacts'><Icon icon='fas fa-cut mg-l--15'/><span className='mg-l-5'>Contacts</span></a></li>
+            <li className="nav_link"><a href='#whyWe'><Icon icon='fas fa-cut mg-l--15'/><span className='mg-l-5'>Why We</span></a></li>
             <li className="nav_link"><a><Icon icon='fas fa-cut mg-l--15'/><span className='mg-l-5'>Services</span></a></li>
             <li className="nav_link"><a><Icon icon='fas fa-user mg-l--15'/><span className='mg-l-5'>Profile</span></a></li>
             <li className="nav_link"><a><Icon icon='fas fa-sign-in-alt mg-l--15'/><span className='mg-l-5'>Log In</span></a></li>
@@ -23,25 +28,43 @@ function App() {
           </ul>
       </nav>
       </header>
-      <main>
-        <div className = "container--flex wrap--wrap">
-          <img className="logo" src={logo}></img>
-          <img className="logo" src={logo}></img>
-          <img className="logo" src={logo}></img>
-          <img className="logo" src={logo}></img>
-          <img className="logo" src={logo}></img>
-          <img className="logo" src={logo}></img>
-          <img className="logo" src={logo}></img>
-          <img className="logo" src={logo}></img>
-          <img className="logo" src={logo}></img>
-          <img className="logo" src={logo}></img>
-          <img className="logo" src={logo}></img>
-          <img className="logo" src={logo}></img>
+      <main  className="main">
+        <div className = "main_content container--flex direction--column justify--between align-between items--between">
+          <section id='contacts' className = "main_presentation presentation mg-t-35 container--flex justify--center align--start items--center direction--column">
+            <h1>Einheria Barbershop</h1>
+            <img className="logo mg-t-15" src={logo}></img>
+            <div className='presentation_info info container--flex direction--row justify--evenly mg-t-35'>
+              <div className='info_adress adress'>
+                <h2>Adress</h2>
+                <p className = 'mg-t-10'>Asgard, Valhalla</p>
+              </div>
+              <div className='info_number number'>
+                <h2>Telephone Number</h2>
+                <p className = 'mg-t-10'>+0-123-456-78-99</p>
+              </div>
+              <div className='info_hours hours'>
+                <h2>Work Hours</h2>
+                <p className = 'mg-t-10'>00:00 -- Ragnarok</p>
+              </div>
+            </div>
+          </section>
+          <section id='whyWe' className = "main_whyWe whyWe container--flex justify-center align--center direction--column">
+            <h1 className = "mg-t-35">Why we?</h1>
+            <p className = "mg-t-15">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <h1 className = "mg-t-45">Our Work Examples</h1>
+            <div className = "whyWe_works mg-t-25 container--flex direction--row justify--around">
+              <div><img className = "works_work work_1" src = {work1}></img></div>
+              <div><img className = "works_work work_2" src = {work2}></img></div>
+              <div><img className = "works_work work_3" src = {work3}></img></div>
+            </div>
+          </section>
 
         </div>
       </main>
-      <footer>
-
+      <footer className = 'footer footer--fixed container--flex direction--row justify--start align--end items--end'>
+        <span>© EPAM, React Project. 2020</span>
       </footer>
 
     </div>
