b54e25f609d014055b6d2b3af609a16a4eae8a64
krivoruchkoND <lizey@bk.ru>
2020-05-20 14:42:23 +0300

    redux init

--- barbershop-landing/src/index.js
+++ barbershop-landing/src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import rootReducer from './Reducers'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+
 import App from './Components/App';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(
+  rootReducer, 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
