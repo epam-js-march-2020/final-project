bdeb48b3d58207bfcfcafa2a021339b8537d7889
krivoruchkoND <lizey@bk.ru>
2020-05-26 12:53:35 +0800

    home page done

--- barbershop-landing/src/Components/Home/Home.jsx
+++ barbershop-landing/src/Components/Home/Home.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
+import Container from 'react-bootstrap/Container';
+
 const Home = () => {
   const [t] = useTranslation();
   return (
-    <div> Home </div>
+    <Container>
+      <h2 className="text-center">{t('home.welcome')}</h2>
+      <hr />
+      <h3 className="text-center">{t('home.team')}</h3>
+      <p>{t('home.about.team')}</p>
+      <h3 className="text-center">{t('home.services')}</h3>
+      <p>{t('home.about.services')}</p>
+      <h3 className="text-center">{t('home.time')}</h3>
+      <p>{t('home.about.time')}</p>
+    </Container>
   );
 }
 
