6227de267349a3a8a14f84c50fe122fe5c72a3c2
krivoruchkoND <lizey@bk.ru>
2020-05-27 08:44:42 +0800

    redux removed :(

--- barbershop-landing/src/index.js
+++ barbershop-landing/src/index.js
@@ -1,28 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import { Provider } from 'react-redux';
 
-import rootReducer from './Reducers'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 import App from './Components/App';
 import * as serviceWorker from './serviceWorker';
 
-const middlewares = [thunk];
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(
-  applyMiddleware(...middlewares)
-));
-
 ReactDOM.render(
   //<React.StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>
   //</React.StrictMode>
   ,document.getElementById('root')
 );
